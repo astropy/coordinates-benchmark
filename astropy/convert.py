70e7ef9c8fb0431ea07813d11a7d1620cd94e62a
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-11-17 17:42:15 +0100

    Updated scripts

--- astropy/convert.py
+++ astropy/convert.py
@@ -9,7 +9,7 @@ f = {}
 f['galactic'] = open('coords_galactic.txt', 'wb')
 f['b1950'] = open('coords_b1950.txt', 'wb')
 
-for i in range(len(data)):
+for i in range(len(data_j2000)):
 
     ra_j2000, dec_j2000 = data_j2000[i,0], data_j2000[i,1]
     fk5 = coord.FK5Coordinates(ra_j2000, dec_j2000, unit='degree')
@@ -20,16 +20,13 @@ for i in range(len(data)):
     f['galactic'].write("%20.15f %20.15f\n" % (l.degrees, b.degrees))
 
     # Convert to B1950
-    fk4 = galactic.fk4
+    fk4 = fk5.fk4
     ra_b1950, dec_b1950 = fk4.ra, fk4.dec
     f['b1950'].write("%20.15f %20.15f\n" % (ra_b1950.degrees,
                                             dec_b1950.degrees))
 
     # Convert to ecliptic
     # Not implemented yet
-
-    # Convert back to J2000
-    # Need ecliptic first
 
 for system in f:
     f[system].close()
