6d6900724c427973f4f660badac2d14bf26b7c03
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-11-17 16:37:30 +0100

    Updated PyAST script

--- pyast/convert.py
+++ pyast/convert.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python
+
+import numpy as np
 import starlink.Ast as Ast
 
-#  Create lists holding values read from the first two columns of the
-#  "coords.txt" file (should be the FK5 J2000 RA and Dec values). Convert
-#  from degrees to radians.
-ra_j2000_fk5 = []
-dec_j2000_fk5 =  []
-fd = open( 'coords.txt', 'r' )
-for row in fd:
-   if not '#' in row:
-      values = row.strip().split(',')
-      ra_j2000_fk5.append( float(values[0])*Ast.DD2R )
-      dec_j2000_fk5.append( float(values[1])*Ast.DD2R )
-fd.close()
+# Read in initial coordinates as J2000 coordinates
+data_j2000 = np.loadtxt('../initial_coords.txt')
+ra_j2000_fk5, dec_j2000_fk5 = data_j2000[:,0], data_j2000[:,1]
 
 #  Create a Frame to describe J2000 FK5 coordinates, and another that
 #  will be used in turn to describe each of the output coordinate systems.
@@ -26,6 +19,7 @@ out_frame = Ast.SkyFrame('Format(1)=hms.5,Format(2)=dms.5,Epoch=2000.0')
 #  describe each one.
 vals = {}
 for system in 'FK4', 'Ecliptic', 'Galactic', 'ICRS':
+
    out_frame.System = system
 
    #  Get the transformation from FK5 J2000 to the current output system.
@@ -35,29 +29,24 @@ for system in 'FK4', 'Ecliptic', 'Galactic', 'ICRS':
    #  the above transformation.
    vals[system] = fk5_to_out.tran( [ra_j2000_fk5,dec_j2000_fk5] )
 
+glon = vals['Galactic'][0]
+glat = vals['Galactic'][1]
+np.savetxt('coords_galactic.txt', zip(np.degrees(glon), 
+                                       np.degrees(glat)), fmt="%20.15f")
 
-#  Write the table of converted values (in degrees) to text file "pyast.txt"
-fd = open( 'pyast.txt', 'w' )
-fd.write("# ra_fk5  dec_fk5 ra_fk4 dec_fk4 elon elat glon glat ra dec\n")
-for (ra_fk5,dec_fk5,ra_fk4,dec_fk4,elon,elat,glon,glat,ra,dec) in zip(
-                                          ra_j2000_fk5, dec_j2000_fk5,
-                                          vals['FK4'][0], vals['FK4'][1],
-                                          vals['Ecliptic'][0], vals['Ecliptic'][1],
-                                          vals['Galactic'][0], vals['Galactic'][1],
-                                          vals['ICRS'][0], vals['ICRS'][1]):
+ra_fk4 = vals['FK4'][0]
+dec_fk4 = vals['FK4'][1]
+np.savetxt('coords_b1950.txt', zip(np.degrees(ra_fk4),
+                                    np.degrees(dec_fk4)), fmt="%20.15f")
 
-   ra_fk5 *= Ast.DR2D
-   dec_fk5 *= Ast.DR2D
-   ra_fk4 *= Ast.DR2D
-   dec_fk4 *= Ast.DR2D
-   elon *= Ast.DR2D
-   elat *= Ast.DR2D
-   glon *= Ast.DR2D
-   glat *= Ast.DR2D
-   ra *= Ast.DR2D
-   dec *= Ast.DR2D
-   fd.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n".format(ra_fk5, dec_fk5,
-                                ra_fk4, dec_fk4, elon, elat, glon, glat, ra, dec ))
-fd.close()
+elon = vals['Ecliptic'][0]
+elat = vals['Ecliptic'][1]
+np.savetxt('coords_ecliptic.txt', zip(np.degrees(elon),
+                                       np.degrees(elat)), fmt="%20.15f")
+
+ra_fk5 = vals['ICRS'][0]
+dec_fk5 = vals['ICRS'][1]
+np.savetxt('coords_j2000.txt', zip(np.degrees(ra_fk5),
+                                    np.degrees(dec_fk5)), fmt="%20.15f")
 
 
