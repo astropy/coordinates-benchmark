b1ac156fb5cab71280354deeb6ae7e15c6c5c572
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-11-17 16:37:52 +0100

    Added Astropy script

---
+++ astropy/convert.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+from astropy import coordinates as coord
+
+# Read in initial coordinates as J2000 coordinates
+data_j2000 = np.loadtxt('../initial_coords.txt')
+
+f = {}
+f['galactic'] = open('coords_galactic.txt', 'wb')
+f['b1950'] = open('coords_b1950.txt', 'wb')
+
+for i in range(len(data)):
+
+    ra_j2000, dec_j2000 = data_j2000[i,0], data_j2000[i,1]
+    fk5 = coord.FK5Coordinates(ra_j2000, dec_j2000, unit='degree')
+
+    # Convert to Galactic coordinates
+    galactic = fk5.galactic
+    l, b = galactic.l, galactic.b
+    f['galactic'].write("%20.15f %20.15f\n" % (l.degrees, b.degrees))
+
+    # Convert to B1950
+    fk4 = galactic.fk4
+    ra_b1950, dec_b1950 = fk4.ra, fk4.dec
+    f['b1950'].write("%20.15f %20.15f\n" % (ra_b1950.degrees,
+                                            dec_b1950.degrees))
+
+    # Convert to ecliptic
+    # Not implemented yet
+
+    # Convert back to J2000
+    # Need ecliptic first
+
+for system in f:
+    f[system].close()
