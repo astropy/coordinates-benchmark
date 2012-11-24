06aac87f8915f6e6793cb9473c41be5bad8accfb
Christoph Deil <Deil.Christoph@gmail.com>
2012-11-24 14:47:52 +0100

    Make looping over observer array more readable

--- pyephem/convert_horizontal.py
+++ pyephem/convert_horizontal.py
@@ -12,7 +12,7 @@ observers = np.recfromcsv('../observers.txt')[:2]
 # We'll store the results here
 results = np.zeros(data_j2000.shape[0] * observers.shape[0], dtype=[('az', 'float64'), ('alt', 'float64')])
 
-for ii, observer in enumerate(observers):
+for ii, (lon, lat, altitude, time, time_mjd, time_tdb) in enumerate(observers):
     for jj, (ra, dec) in enumerate(data_j2000):
         # We need to create a "Body" in pyephem, which represents the coordinate
         # http://stackoverflow.com/questions/11169523/how-to-compute-alt-az-for-given-galactic-coordinate-glon-glat-with-pyephem
@@ -23,16 +23,21 @@ for ii, observer in enumerate(observers):
 
         # Set observer parameters
         obs =  ephem.Observer()
-        obs.lon = np.radians(observer[0])
-        obs.lat = np.radians(observer[1])
-        obs.elevation = observer[2]
+        obs.lon = np.radians(lon)
+        obs.lat = np.radians(lat)
+        obs.elevation = altitude
         obs.epoch = '2000'
-        obs.date = observer[3]
+        obs.date = time
+        # Turn refraction off by setting pressure to zero
+        obs.pressure = 0
 
         # Compute alt / az of the body for that observer
         body.compute(obs)
         az, alt = np.degrees([body.az, body.alt])
-        results[ii * data_j2000.shape[0] + jj]['az'] = az
-        results[ii * data_j2000.shape[0] + jj]['alt'] = alt
+
+        # Store in results array
+        kk = ii * data_j2000.shape[0] + jj
+        results[kk]['az'] = az
+        results[kk]['alt'] = alt
 
 np.savetxt('coords_fk5_to_horizontal.txt', results, fmt="%20.15f")
