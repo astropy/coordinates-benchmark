1633675dfbb19d73264d8fdea99215b33d7e1002
Thomas Robitaille <thomas.robitaille@gmail.com>
2012-12-16 19:21:03 +0100

    Fixed bug in SLALIB tests

--- pyslalib/convert.py
+++ pyslalib/convert.py
@@ -19,7 +19,7 @@ def convert(coords, systems):
 
         # First convert to FK5 J2000 in all cases
         if systems['in'] == 'fk4':
-            lon, lat = slalib.sla_fk45z(lon, lat, 1950)
+            lon, lat = slalib.sla_fk45z(lon, lat, 2000.0012775136652)
         elif systems['in'] == 'icrs':
             lon, lat = slalib.sla_hfk5z(lon, lat, 2000)[:2]
         elif systems['in'] == 'galactic':
@@ -30,7 +30,7 @@ def convert(coords, systems):
         # Now convert from FK5 J2000 to out system    
         if systems['out'] == 'fk4':
             # FK5 -> FK4 at BEPOCH 2000 assuming no proper motion or parallax
-            lon, lat = slalib.sla_fk54z (lon, lat, 2000)[:2]
+            lon, lat = slalib.sla_fk54z (lon, lat, 2000.0012775136652)[:2]
         elif systems['out'] == 'icrs':
             # FK5 -> Hipparcos (i.e. ICRF, which is as close as SLALIB
             # gets to ICRS) at epoch 2000 and with no proper motion
