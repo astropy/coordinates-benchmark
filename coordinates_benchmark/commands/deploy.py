757f8a54edfeb2744fe0eb4dfd7d93de73e6ffa4
Thomas Robitaille <thomas.robitaille@gmail.com>
2017-08-20 14:56:47 +0100

    Updated deploy command to only add files that have really changed

--- coordinates_benchmark/commands/deploy.py
+++ coordinates_benchmark/commands/deploy.py
@@ -2,11 +2,13 @@
 from __future__ import absolute_import, division, print_function
 
 import os
-import sys
 import shutil
 import tempfile
 import subprocess
+import filecmp
+
 import click
+from matplotlib.testing.compare import compare_images
 
 
 def run(cmd):
@@ -15,24 +17,57 @@ def run(cmd):
     assert retcode == 0
 
 
+def copy_mkdir(original, destination):
+    directory = os.path.dirname(destination)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    shutil.copy2(original, destination)
+
+
 @click.command()
 @click.option('--repo', default='astropy/coordinates-benchmark', help='Github repo slug')
 def deploy(repo):
     """Deploy HTML output to Github pages."""
 
-    tmpdir = tempfile.mkdtemp()
-    tmpout = os.path.join(tmpdir, 'output')
-    shutil.copytree('output', tmpout)
+    tmp_dir = tempfile.mkdtemp()
     start_dir = os.path.abspath('.')
+    output_dir = os.path.abspath('output')
 
     try:
-        os.chdir(tmpout)
+        os.chdir(tmp_dir)
         run('git init')
         run('git remote add upstream git@github.com:{0}'.format(repo))
         run('git fetch upstream')
         run('git reset upstream/gh-pages')
-        run('git add -A .')
-        run('git commit --allow-empty -m "Latest build"')
-        run('git push -q upstream HEAD:gh-pages')
+
+        # We check the files one by one because we want to minimize changes and
+        # make sure we only upload plots that have really changed (otherwise
+        # PNG files look externally different every time they are generated even
+        # if the real content hasn't changed)
+
+        changed = False
+
+        for root, _, filenames in os.walk(output_dir):
+            for filename in filenames:
+                original_file = os.path.join(root, filename)
+                new_file = os.path.relpath(original_file, output_dir)
+                if os.path.exists(new_file):
+                    if filename.endswith('.png'):
+                        if compare_images(original_file, new_file, tol=0) is not None:
+                            copy_mkdir(original_file, new_file)
+                            changed = True
+                    else:
+                        if not filecmp.cmp(original_file, new_file, shallow=False):
+                            copy_mkdir(original_file, new_file)
+                            changed = True
+                else:
+                    copy_mkdir(original_file, new_file)
+                    changed = True
+
+        if changed:
+            run('git add -A .')
+            run('git commit --allow-empty -m "Latest build"')
+            run('git push -q upstream HEAD:gh-pages')
+
     finally:
         os.chdir(start_dir)
